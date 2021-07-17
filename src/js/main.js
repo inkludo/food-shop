618d2cfde1a776eaee30f9f7fb1a8be72d24012e
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-07-17 15:14:44 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector(".tabheader__items");
@@ -38,5 +40,60 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    //Timer
+
+    const deadline = '2021-07-25';
+
+    function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor((total / (1000 * 60 * 60 * 24))),
+            hours = Math.floor((total / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor(((total / 1000 / 60) % 60)),
+            seconds = Math.floor(((total / 1000) % 60));
+
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInerval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInerval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 
 });
